c5f84fc3339be9b704593dddde6aae65cb37b7b4
Akhil Puri <ap4723@srmist.edu.in>
2026-02-13 12:53:30 +0530

    [Akhil] Added: UC1: Display a static message

---
+++ LexicalTwistPuzzle.java
@@ -0,0 +1,15 @@
+/**
+ * LexicalTwistPuzzle UC1: Display Puzzle Title
+ *
+ * This class Display a static message indicating the purpose of the application.
+ *
+ * @Developer
+ * @version 1.0
+ * */
+
+
+public class LexicalTwistPuzzle {
+    public static void main(String[] args) {
+        System.out.println("Lexical Twist Puzzle");
+    }
+}
